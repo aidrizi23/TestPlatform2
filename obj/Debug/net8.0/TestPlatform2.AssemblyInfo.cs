4fe318684fa8c62ffb0aa856960da61bcf7de211
Albi Idrizi <albiidrizi27@gmail.com>
2025-02-02 03:21:18 +0100

    Change style of most of the views

--- obj/Debug/net8.0/TestPlatform2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestPlatform2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8452ba85035322928572d2ee1e32a18aa402c3e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb89c208acdb43c0a14e3a8639114b4161a29f9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
