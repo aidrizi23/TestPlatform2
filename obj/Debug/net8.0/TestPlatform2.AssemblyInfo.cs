7a1b23d85ddbb222358227006f3b96af52e39a70
Albi Idrizi <albiidrizi27@gmail.com>
2025-05-27 13:00:15 +0200

    sub fix

--- obj/Debug/net8.0/TestPlatform2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestPlatform2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7359de57fa3911b2692ed2cf37a96c4cc9c9999d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed48ada179c95be8ad4edac7cced6461d3dd1da8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
