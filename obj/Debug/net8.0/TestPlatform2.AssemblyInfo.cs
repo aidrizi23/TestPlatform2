e7a0561f0013ba13af6bfc61edea58a23d578be5
Albi Idrizi <albiidrizi27@gmail.com>
2025-06-15 13:25:23 +0200

    fix archive button

--- obj/Debug/net8.0/TestPlatform2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestPlatform2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b201856ec08b290066591fe54f6d1ed24e204009")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af32403dc486a7c2554382dc2e6d53ac5075f7dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
