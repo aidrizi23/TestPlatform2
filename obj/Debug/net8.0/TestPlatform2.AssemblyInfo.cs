8cb9d909e15f92350b95ab956e10eabf5f4c169a
Albi Idrizi <albiidrizi27@gmail.com>
2025-06-15 13:35:04 +0200

    fix views

--- obj/Debug/net8.0/TestPlatform2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestPlatform2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af32403dc486a7c2554382dc2e6d53ac5075f7dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a0561f0013ba13af6bfc61edea58a23d578be5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestPlatform2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
